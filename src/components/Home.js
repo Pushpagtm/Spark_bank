11bf09077bb3731b5ec4b22ab7ae7be4f0a9d86f
Pushpagtm <pushpagautam529@gmail.com>
2023-06-02 09:36:02 +0545

    work on home page

---
+++ src/components/Home.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import '../styles/home.css';
+function Home(props) {
+    return (
+        <div>
+          
+        <div className='container'>
+        <div className="left">
+                <div className="title">
+                    <h2>The Spark Bank Limited</h2>
+                </div>
+                <div className="desc">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi debitis quia cumque possimus, doloremque quaerat! Perferendis et eveniet, praesentium mollitia aliquam dolore placeat dolores architecto assumenda veritatis at laboriosam soluta debitis tempora.</p>
+                </div>
+                <button className='start-btn'>
+                    <a href="/customer">
+                        Get Started
+                    </a>
+                </button>
+            </div>
+            
+            
+
+
+        </div>
+            
+        </div>
+    );
+}
+
+export default Home;
---
+++ src/components/TransferMoney.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function TransferMoney(props) {
+    return (
+        <div>
+            mnvbdhsfbh
+        </div>
+    );
+}
+
+export default TransferMoney;
