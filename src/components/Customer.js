01a2bfe361e39587f66e89469b0eb80ffa557d14
Pushpagtm <pushpagautam529@gmail.com>
2023-06-06 08:22:20 +0545

    deleted files

--- src/components/Customer.js
+++ src/components/Customer.js
@@ -1,40 +1,17 @@
 import React, { useEffect, useState } from "react";
 import "../styles/customer.css";
-import { collection, getDocs } from 'firebase/firestore'
-import firebase from "../firebase";
+
 function Customer(props) {
   const [data, setData] = useState([]);
   
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await firebase
-          .firestore()
-          .collection("customers")
-          .get();
-        const fetchedData = response.docs.map((doc) => doc.data());
-        setData(fetchedData);
-
-        // const collectionsRef = firebase.firestore().listCollections();
-        // const fetchedCollections = await collectionsRef;
-        // const collectionNames = fetchedCollections.map((collection) => collection.id);
-
-        console.log("all the collections", fetchedData);
-        // setData(collectionNames);
-
-        //   console.log(fetchData,"fetching datas===============================")
-        //   setData(fetchedData);
-      } catch (error) {
-       console.error("Error occured....",error)
-      }
-    };
-    fetchData();
+    
   }, []);
 
   return (
     <>
-      {console.log(data, "=====================all data")}
+   
       <p>fetched data</p>
 
       {/* <div >
